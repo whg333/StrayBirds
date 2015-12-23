b7baff4f0f61441780456b51f10b12c21d0d32ff
wanghonggang <wanghonggang@hoolai.com>
2015-12-23 17:39:38 +0800

    通过新窗口打开不是本站的连接

--- js/kunka.js
+++ js/kunka.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	//console.log("jquery work!");
 	
-	//通过新窗口打开链接
+	//通过新窗口打开不是本站的连接
 	$(document.links).filter(function() {
 	    return this.hostname != window.location.hostname;
 	}).attr('target', '_blank');
