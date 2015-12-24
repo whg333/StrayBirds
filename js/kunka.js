ea65f7d53a7760d2dd5be4e35e93b99039ece82d
wanghonggang <wanghonggang@hoolai.com>
2015-12-24 14:54:03 +0800

    点击图片可打开新窗口查看原图

--- js/kunka.js
+++ js/kunka.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
 	//console.log("jquery work!");
 	
-	//通过新窗口打开不是本站的连接
+	//通过新窗口打开不是本站的链接
 	$(document.links).filter(function() {
 	    return this.hostname != window.location.hostname;
 	}).attr('target', '_blank');
+	
+	//点击图片可打开新窗口查看原图
+	$(".main .article img").click(function(){
+		window.open($(this).attr('src'));
+        return false;
+	});
 });
